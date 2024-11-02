e0f9cf252d5a4b3a220d20f362e8f553ea856530
e33io <info@e33.io>
2024-11-02 14:31:26 -0500

    Update URLs

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<!-- evo wp theme - https://git.sr.ht/~e33io/evo-wp-theme -->
+<!-- evo wp theme - https://github.com/e33io/evo-wp-theme -->
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="description" content="<?php bloginfo( 'description' ); ?> - all content &copy; <?php bloginfo( 'name' ); ?> - all rights reserved" />
